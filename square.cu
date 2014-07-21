918e6e1717b283d48fdc9a6752e720c64189b45d
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2014-07-20 23:38:22 -0600

    Full square program

--- square.cu
+++ square.cu
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 __global__ void square(float *d_out, float *d_in) {
   int idx = threadIdx.x;
@@ -23,3 +24,25 @@ int main(int argc, char **argv) {
   //allocate GPU memory
   cudaMalloc((void **) &d_in, ARRAY_BYTES);
   cudaMalloc((void **) &d_out, ARRAY_BYTES);
+
+  //transfer the array to the GPU
+  cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+
+  //launch the kernel
+  cube<<<1, ARRAY_SIZE>>>(d_out, d_in);
+
+  //copy back the result array to the CPU
+  cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+
+  //print out the resulting array
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    printf("%f", h_out[i]);
+    printf(((i % 4) != 3) ? "\t" : "\n");
+  }
+
+  cudaFree(d_in);
+  cudaFree(d_out);
+
+  return 0;
+}
+
